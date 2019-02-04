a3d26268f9ed96ff5d9c7fec2c3f715107ab858c
theachyutkadam <theachyutkadam@gmail.com>
2019-02-04 15:58:34 +0530

    distrubute code in many defination

--- school_operation/school.rb
+++ school_operation/school.rb
@@ -1,8 +1,10 @@
+
 class Classroom
-  def create_class
+
+  @@classroom = []
+  def process_classroom_creation
     print "How many class are you like to Add : "
     number_of_class = gets.chomp.to_i
-    @@classroom = []
     $school_count = @@classroom
     i = 0
     while i < number_of_class
@@ -11,19 +13,27 @@ class Classroom
       @@classroom << c_name
       i +=1
     end
+    choice_messages_of_classroom
+  end
+
+  def choice_messages_of_classroom
     puts "You Add Classroom successfully"
     puts " 1 Display ClassRoom : "
     puts " 2 See Menu list : "
     puts " 3 Exit : "
     print "Enter Your Choice : "
-    n = gets.chomp.to_i
+    choice_value = gets.chomp.to_i
+    operation_on_variable(choice_value)
+  end
+
+  def operation_on_variable(choice_value)
+    n= choice_value
     case n
     when 1
-      obj_classroom = Classroom.new
-      obj_classroom.display_class
+      display_class
     when 2
-      obj_school_info = School.new
-      obj_school_info.choice_number
+      obj_school=School.new
+      obj_school.select_value_for_next_operation
     when 3
       puts "Thanks You"
     else
@@ -34,20 +44,20 @@ class Classroom
   end
 
   def display_class
-    puts "Your Class Details : "
     if @@classroom.nil?
-      puts "Enter first classroom details: "
-      obj_classroom = Classroom.new
-      obj_classroom.display_class
-    else
+       puts "Your Class Details : "
       @@classroom.each {|cls| puts "#{cls}"}
+    else
+      puts "Enter first classroom details: "
+      process_classroom_creation
     end
   end
+
 end
 
 class Student
+  @@student_info = []
   def create_student
-    @@student_info = []
     puts "Add Student Details : "
     print "Select Student ClassRoom using 1 To #{$school_count.count} : "
     classroom_count = gets.to_i
@@ -61,25 +71,32 @@ class Student
     stud_mark = gets.chomp.to_f
     stud_info_hash = {stud_name: stud_name, stud_age: stud_age, stud_mark: stud_mark, classroom: classroom}
     @@student_info.push(stud_info_hash)
-    puts "-------------Student List-------------------"
     @@student_info.each {|attri, val| puts " #{attri} = #{val}"}
-    puts "--------------------------------"
-    School.new.choice_number
+    School.new.select_value_for_next_operation
   end
 
   def show_student_info
-    puts " 1 Display All Student : "
-    puts " 1 Display classroom student : "
-    puts " 2 See Menu List : "
-    puts " 3 Exit : "
-    print "Enter Your Choice : "
-    n = gets.chomp.to_i
+    if @@student_info.nil?
+      puts "Enter firsts Student Information : "
+      obj_student.create_student
+    else
+      puts " 1 Display All Student : "
+      puts " 1 Display classroom student : "
+      puts " 2 See Menu List : "
+      puts " 3 Exit : "
+      print "Enter Your Choice : "
+      stud_info = gets.chomp.to_i
+      operation_on_student_list(stud_info)
+    end
+  end
+
+  def operation_on_student_list(stud_info)
+    n = stud_info
     case n
     when 1
       @@student_info.each {|attri, val| puts " #{attri} = #{val}"}
     when 2
-      obj_school_info = School.new
-      obj_school_info.choice_number
+      select_value_for_next_operation
     when 3
       puts "Thanks You"
     else
@@ -91,31 +108,37 @@ class Student
 end
 
 class School
-  puts "*****Welcome To SCHOOL PROJECT******"
-  i = 0
-  print "Enter your school name : "
-  $school_name = gets
-  puts "The School Name is : #{$school_name.capitalize}"
-  def choice_number
+
+  def school_info
+    print "Enter your school name : "
+    $school_name = gets
+    puts "The School Name is : #{$school_name.capitalize}"
+    select_value_for_next_operation
+  end
+
+  def select_value_for_next_operation
     puts " 1 Add New ClassRoom : "
     puts " 2 Add New Student : "
     puts " 3 List of ClassRoom : "
     puts " 4 List of Student : "
     puts
     print "Enter Your Choice : "
-    n = gets.chomp.to_i
+    selected_choice = gets.chomp.to_i
+    operation_on_selected_number(selected_choice)
+  end
+
+  def operation_on_selected_number(selected_choice)
+    n = selected_choice
+    obj_class = Classroom.new
+    obj_student = Student.new
     case n
     when 1
-      obj_classroom = Classroom.new
-      obj_classroom.create_class
+      obj_class.process_classroom_creation
     when 2
-      obj_student = Student.new
       obj_student.create_student
     when 3
-      obj_classroom = Classroom.new
-      obj_classroom.display_class
+      obj_class.display_class
     when 4
-      obj_student = Student.new
       obj_student.show_student_info
     else
       if n >=4 || n <=0
@@ -123,7 +146,8 @@ class School
       end
     end
   end
+
 end
 
 obj_school = School.new
-obj_school.choice_number
+obj_school.school_info
