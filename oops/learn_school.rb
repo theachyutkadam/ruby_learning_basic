aeae0faf0b32994b29c54ccf81cb89491b2cb2b7
theachyutkadam <theachyutkadam@gmail.com>
2019-02-09 15:59:04 +0530

    sort student by their attributes

--- oops/learn_school.rb
+++ oops/learn_school.rb
@@ -30,6 +30,7 @@ class Student
     @name = name
     @marks = marks
     @birthdate = birthdate
+    add
   end
   @@information = []
   def add
@@ -41,22 +42,27 @@ class Student
   def list_student_n_classroom
     p "list_student_n_classroom"
   end
-  def sort_by_name
-    @@information.key(@name.to_s)
+  def sort_by_name=(name)
+    p @@information.find {|x| x[:name] == name}
   end
-  def sort_by_marks
-    p "sort_by_marks"
+  def sort_by_marks=(marks)
+    p @@information.find {|x| x[:marks] == marks}
   end
-  def sort_by_birthdate
-    p "sort_by_birthdate"
+  def sort_by_birthdate=(birthdate)
+    @@information.find do |x|
+      if x[:birthdate] == birthdate
+        puts "#{x}"
+      end
+    end
   end
 end
 
-student = Student.new("achyut", 87, "1996-07-02")
-student = Student.new("kadam", 78, "2012-10-10")
-student.add
-student.sort_by_name
-student.sort_by_marks
-student.sort_by_birthdate
+student = Student.new("achyut", 77, "1996-07-02")
+student = Student.new("kadam", 64, "2000-10-10")
+student = Student.new("surya", 80, "1992-10-10")
+student.sort_by_name = "kadam"
+# student.add
+student.sort_by_marks = 64
+student.sort_by_birthdate = "1992-10-10"
 # student.add_multiple_student=(5)
 student.list_student_n_classroom
