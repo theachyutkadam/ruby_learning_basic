1672799144440838be18d1a59ee872595f247f82
theachyutkadam <theachyutkadam@gmail.com>
2019-02-11 01:59:55 +0530

    make a user friendly

--- oops/learn_school.rb
+++ oops/learn_school.rb
@@ -1,84 +1,98 @@
-  class Student
-    def initialize(name, marks, birthdate)
-      @name = name
-      @marks = marks
-      @birthdate = birthdate
-    end
+class School
+  def initialize name
+    @name, @classrooms = [], []
+    @name.push(name)
   end
 
-  class ClassRoom
-    def initialize(name)
-      @name = name
-      @students = []
-    end
-    def add_student(new_student)
-      @students << new_student
-    end
-    def show_students_by_classroom
-      @students
-    end
-    def sort_student(student)
-      @students.each do |x|
-        if x == student
-          p x
-        end
-      end
-    end
+  def add_classroom new_classroom
+    @classrooms << new_classroom
   end
 
-  class School
-    def initialize(name)
-      @name = []
-      @name.push(name)
-      @classrooms = []
-    end
-    def add_classroom(new_classroom)
-      @classrooms << new_classroom
-    end
-    def show_classroom(classroom_name)
-      @classrooms.find(classroom_name)
-    end
-    def show_classrooms
-      @classrooms
-    end
-    def school_details(school)
-      p school
-    end
+  def show_classroom classroom_name
+    p @classrooms.find(classroom_name)
   end
-  school_1 = School.new("Ruby School")
-  school_1.school_details(school_1)
 
-  classroom_1 = ClassRoom.new("Beginners")
-  classroom_2 = ClassRoom.new("Graduate")
-  classroom_3 = ClassRoom.new("Professional")
+  def show_classrooms
+    @classrooms.each {|classroom| p classroom }
+  end
+end
 
-  student_1 = Student.new("AA_1", 71, "1991-04-01")
-  student_2 = Student.new("BB_2", 62, "1992-05-02")
-  student_3 = Student.new("CC_3", 83, "1993-06-03")
-  student_4 = Student.new("DD_4", 74, "1994-07-04")
-  student_5 = Student.new("EE_5", 65, "1995-08-05")
-  student_6 = Student.new("FF_6", 86, "1996-09-06")
-  student_7 = Student.new("GG_7", 77, "1997-10-07")
-  student_8 = Student.new("HH_8", 68, "1998-11-08")
-  student_9 = Student.new("II_9", 89, "1999-12-09")
 
-  school_1.add_classroom(classroom_1)
-  school_1.add_classroom(classroom_2)
-  school_1.add_classroom(classroom_3)
+class ClassRoom
+  def initialize(name)
+    @name, @students = name, []
+  end
 
-  school_1.show_classroom(classroom_1)
-  school_1.show_classroom(classroom_2)
-  school_1.show_classroom(classroom_3)
-  school_1.show_classrooms
+  def add_student(new_student)
+    @students << new_student
+  end
 
-  classroom_1.add_student(student_1)
-  classroom_1.add_student(student_2)
-  classroom_1.add_student(student_3)
-  classroom_2.add_student(student_4)
-  classroom_2.add_student(student_5)
-  classroom_2.add_student(student_6)
-  classroom_3.add_student(student_7)
-  classroom_3.add_student(student_8)
-  classroom_3.add_student(student_9)
-  classroom_1.show_students_by_classroom
-  classroom_1.sort_student(student_1)
+  def show_students_classroom
+    @students.each {|student| p student}
+  end
+
+  def sort_student(student)
+    @students.each { |x| p x if x == student }
+  end
+end
+
+class Student
+  def initialize(name, marks, birthdate)
+    @name, @marks, @birthdate = name, marks, birthdate
+  end
+end
+
+while true
+  print "----------------------------------------------------------\n\n"
+  puts "Select Options"
+  puts "1. Create School"
+  puts "2. Add ClassRooms to School"
+  puts "3. List ClassRooms of School"
+  puts "4. Add Students to School"
+  puts "5. List Students of ClassRooms"
+  puts "6. Sort Students as per Name"
+  # puts "5.2. Sort Students as per Marks"
+  # puts "5.3. Sort Students as Birthdate"
+  puts "0. Exit\n"
+
+  print "Enter your Choice: "
+  choice = gets.chomp
+
+  case choice
+  when "1"
+    print "Please specify school name: "
+    school_name = gets.chomp
+    school = School.new(school_name)
+    puts "School #{school_name} created successfully..."
+  when "2"
+    print "Add classrooms to school: "
+    classroom_name = gets.chomp
+    classroom = ClassRoom.new(classroom_name)
+    school.add_classroom(classroom)
+    puts "Classroom #{classroom_name} created successfully..."
+  when "3"
+    print "Show classrooms to school: \n"
+    school.show_classrooms
+  when "4"
+    print "Select Classroom: "
+    classroom = gets.chomp
+    print "Student Name: ".
+    name = gets.chomp
+    print "Student Marks: ".
+    marks = gets.chomp
+    print "Student Birthdate e.g(YYYY-MM-DD): ".
+    birthdate = gets.chomp
+    student = Student.new(name, marks, birthdate)
+    puts student
+    classroom.add_student(student)
+  when "5"
+    print "List Students of ClassRooms: \n"
+    classroom.show_students_classroom
+  when "6"
+    print "Sort Student by Name: ".
+    student_name = gets.chomp
+    classroom.sort_student(student_name)
+  when "0"
+    exit
+  end
+end
