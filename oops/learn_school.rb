c604c2dc8b9541eacd39ede200fd36afacca03e0
chetan thakkar <chetan.thakkar@definelabs.com>
2019-02-12 02:51:54 +0530

    add n student

--- oops/learn_school.rb
+++ oops/learn_school.rb
@@ -18,7 +18,11 @@ class School
   end
 
   def show_classroom classroom_name
-    @classrooms.find {|cm| p cm if cm[:classroom] == classroom_name }
+    @classrooms.find do |cm|
+      if cm[:classroom] == classroom_name
+        p cm 
+      end
+    end
   end
 
   def show_classrooms
@@ -31,7 +35,8 @@ class ClassRoom
   def initialize
   end
 
-  def self.add_student new_student
+  def self.add_student student, classroom
+    new_student = {student: student, classroom: classroom}
     @@students << new_student
   end
 
@@ -39,8 +44,13 @@ class ClassRoom
     @@students.each {|student| p student}
   end
 
-  def sort_student student
-    @@students.each { |x| p x if x == student }
+  def sort_student classroom_name
+    # @@students.each { |x| p x if x == student }
+    @@students.find do |stud|
+      if stud[:classroom] == classroom_name
+        p stud
+      end
+    end
   end
 end
 
@@ -52,72 +62,97 @@ end
 
 while true
   print "----------------------------------------------------------\n\n"
-  puts "Select Options"
+  puts "Select Options Using Number"
   puts "1. Create School"
   puts "2. Show School"
   puts "3. Add ClassRooms to School"
   puts "4. List ClassRooms of School"
   puts "5. Find ClassRooms of School"
-  puts "6. Add Students to School"
-  puts "7. List Students of ClassRooms"
-  puts "8. Sort Students as per Name"
+  puts "6. Add Student to School"
+  puts "7. Add N Students to School"
+  puts "8. List Students of ClassRooms"
+  puts "9. Sort Students by Classroom"
   puts "0. Exit\n"
 
   print "Enter your Choice: "
-  choice = gets.chomp
+  choice = gets.chomp.to_i
 
   case choice
 
-  when "1"
+  when 1
     print "Please specify school name: "
     school_name = gets.chomp
     school = School.new
     School.add(school_name)
     puts "School #{school_name} created successfully..."
 
-  when "2"
-    puts "School Name: "
+  when 2
+    puts "School Names: "
     school.show
 
-  when "3"
+  when 3
     school.show
-    print "Which School to Add Classroom: "
+    print "Select School for Add Classroom: "
     school_name = gets.chomp
     print "Enter classroom Name: "
     classroom_name = gets.chomp
     school.create_classroom(classroom_name, school_name)
     puts "Classroom #{classroom_name} created successfully..."
 
-  when "4"
+  when 4
     school.show_classrooms
 
-  when "5"
+  when 5
     school.show_classrooms
     print "Enter Classroom Name: "
     classroom_name = gets.chomp
     school.show_classroom(classroom_name)
 
-  when "6"
-    print "Student Name: "
+  when 6
+    print "Enter Student Name: "
     name = gets.chomp
-    print "Student Marks: "
+    print "Enter Student Marks: "
     marks = gets.chomp
-    print "Student Birthdate e.g(YYYY-MM-DD): "
+    print "Enter Student Birthdate e.g(YYYY-MM-DD): "
     birthdate = gets.chomp
+    school.show_classrooms
+    print "Select Classroom Name: "
+    classroom_name = gets.chomp
     student = Student.new(name, marks, birthdate)
-    puts student
     classroom = ClassRoom.new
-    ClassRoom.add_student(student)
+    ClassRoom.add_student(student, classroom_name)
+    puts "Student Add successfully..."
 
-  when "7"
+  when 7
+    print "How Many Students You Add in ClassRooms: "
+    number_student = gets.chomp.to_i
+    for i in 1..number_student do
+      print "Enter #{i} Number Student Name: "
+      name = gets.chomp
+      print "Enter #{i} Number Student Marks: "
+      marks = gets.chomp
+      print "Enter #{i} Number Student Birthdate e.g(YYYY-MM-DD): "
+      birthdate = gets.chomp
+      school.show_classrooms
+      print "Select Classroom Name: "
+      classroom_name = gets.chomp
+      student = Student.new(name, marks, birthdate)
+      classroom = ClassRoom.new
+      ClassRoom.add_student(student, classroom_name)    
+      puts "Student Add successfully..."
+    end
+  when 8
     print "List Students of ClassRooms: \n"
     classroom.show_students_classroom
 
-  when "8"
-    print "Sort Student by Name: "
-    student_name = gets.chomp
-    classroom.sort_student(student_name)
-  when "0"
+  when 9
+    school.show_classrooms
+    print "Sort by Classroom, Select Classroom Name: "
+    classroom_name = gets.chomp
+    classroom.sort_student(classroom_name)
+  when 0
     exit
+  else
+    puts "Invalid Input!!!" if choice.class != Fixnum || choice < 0 || choice >= 8
   end
 end
