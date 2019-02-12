20c20e797469f8a8a5df4272d236690a7b35e8df
chetan thakkar <chetan.thakkar@definelabs.com>
2019-02-12 21:09:25 +0530

    Task 6 Implement interface to add 1 Student in ClassRoom

--- oops/learn_school_v2.rb
+++ oops/learn_school_v2.rb
@@ -28,8 +28,18 @@ class Classroom
     @name = name
     @student = []
   end
+
+  # create new student
+  def add_student(student)
+    @student << student
+  end
 end
 
+class Student
+  def initialize(name, age, birthdate)
+    @name, @age, @birthdate = name, age, birthdate
+  end
+end
 
 
 
@@ -37,9 +47,9 @@ end
 # 1. Create Class Diagram with Data Types only - Done
 # 2. Add interfaces in Class Diagram - Done
 # 3. Implement interface to add School - Done
-# 4. Implement interface to add ClassRoom
-# 5. Implement interface to List ClassRooms of School
-# 6. Implement interface to add 1 Student in ClassRoom
+# 4. Implement interface to add ClassRoom - uncheck
+# 5. Implement interface to List ClassRooms of School - uncheck
+# 6. Implement interface to add 1 Student in ClassRoom - uncheck
 # 7. Implement interface to add n  Students in ClassRoom
 # 8. Implement interface to list Students of ClassRoom
 # 9. Implement interface to sort Students of ClassRoom
@@ -82,3 +92,23 @@ end
 # school.list_classrooms => [#<Classroom:0x0000000266ab00 @name="beginners", @student=[]>, #<Classroom:0x00000002651cb8 @name="graduation", @student=[]>, #<Classroom:0x000000026336a0 @name="professional", @student=[]>]
 #
 #
+# 6. Implement interface to add 1 Student in ClassRoom
+# --- Execute---
+# school = School.new("ruby") => #<School:0x00000002315f90 @name="ruby", @classrooms=[]>
+# School.add(school) => [#<School:0x00000002315f90 @name="ruby", @classrooms=[]>]
+# School.all => [#<School:0x00000002315f90 @name="ruby", @classrooms=[]>]
+# classroom1 = Classroom.new("beginners") => #<Classroom:0x000000022f3760 @name="beginners", @student=[]>
+# classroom2 = Classroom.new("graduation") => #<Classroom:0x000000026f9fa8 @name="graduation", @student=[]>
+# school.add_classroom(classroom1) => [#<Classroom:0x000000022f3760 @name="beginners", @student=[]>]
+# school.add_classroom(classroom2) => [#<Classroom:0x000000022f3760 @name="beginners", @student=[]>, #<Classroom:0x000000026f9fa8 @name="graduation", @student=[]>]
+# school.list_classrooms => [#<Classroom:0x000000022f3760 @name="beginners", @student=[]>, #<Classroom:0x000000026f9fa8 @name="graduation", @student=[]>]
+# student1 = Student.new("achyut", 23, "1996-07-02") => #<Student:0x00000002671d60 @name="achyut", @age=23, @birthdate="1996-07-02"> 
+# student2 = Student.new("surya", 27, "1991-05-16") => #<Student:0x00000002653bd0 @name="surya", @age=27, @birthdate="1991-05-16">
+# student3 = Student.new("satish", 20, "1999-07-01") => #<Student:0x0000000263e870 @name="satish", @age=20, @birthdate="1999-07-01">
+# classroom1.add_student(student1) => [#<Student:0x00000002671d60 @name="achyut", @age=23, @birthdate="1996-07-02">]
+# classroom1.add_student(student2) => [#<Student:0x00000002671d60 @name="achyut", @age=23, @birthdate="1996-07-02">, #<Student:0x00000002653bd0 @name="surya", @age=27, @birthdate="1991-05-16">]
+# classroom2.add_student(student3) => [#<Student:0x0000000263e870 @name="satish", @age=20, @birthdate="1999-07-01">]
+
+# school => #<School:0x00000002315f90 @name="ruby", @classrooms=[#<Classroom:0x000000022f3760 @name="beginners", @student=[#<Student:0x00000002671d60 @name="achyut", @age=23, @birthdate="1996-07-02">, #<Student:0x00000002653bd0 @name="surya", @age=27, @birthdate="1991-05-16">]>, #<Classroom:0x000000026f9fa8 @name="graduation", @student=[#<Student:0x0000000263e870 @name="satish", @age=20, @birthdate="1999-07-01">]>]>
+# classroom1 => Classroom:0x000000022f3760 @name="beginners", @student=[#<Student:0x00000002671d60 @name="achyut", @age=23, @birthdate="1996-07-02">, #<Student:0x00000002653bd0 @name="surya", @age=27, @birthdate="1991-05-16">]>
+# classroom2 => #<Classroom:0x000000026f9fa8 @name="graduation", @student=[#<Student:0x0000000263e870 @name="satish", @age=20, @birthdate="1999-07-01">]>
