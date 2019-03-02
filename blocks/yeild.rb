97616633798e1573e3d96ee806c5258c4eab57a2
theachyutkadam <theachyutkadam@gmail.com>
2019-03-02 16:25:40 +0530

    yeild statement program

--- blocks/yeild.rb
+++ blocks/yeild.rb
@@ -1,14 +1,37 @@
 def my_method
-  puts "reached the top"
+  puts "before yield"
   yield
-  puts "reached the bottom"
+  puts "after yield"
 end
-
 my_method do
   puts "reached yield"
 end
 
 # output
-# reached the top
+# before yield
+# after yield
 # reached yield
-# reached the bottom
+
+def main
+	value = yield
+	puts "this is #{value}"
+end
+
+main do
+	2
+end #pass value by yield
+
+def a_method(&block)
+  p block
+end
+
+a_method { "x" }
+
+
+
+a_proc = Proc.new { "x" }
+a_method(&a_proc) # call using object
+
+# yield = this statement is use to return a value agin function call location..
+
+
